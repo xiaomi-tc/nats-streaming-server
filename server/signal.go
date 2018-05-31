10b96639301530bf9de7a37dfedacca620b48f65
steven <steven@wodedagong.com>
2018-05-31 21:17:51 +0800

    add QUIT signal handle

--- server/signal.go
+++ server/signal.go
@@ -12,15 +12,17 @@ import (
 // Signal Handling
 func (s *StanServer) handleSignals() {
 	c := make(chan os.Signal, 1)
+
+	//2017-12-26
 	//signal.Notify(c, syscall.SIGINT, syscall.SIGUSR1)
-	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL, syscall.SIGHUP,syscall.SIGUSR1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGHUP,syscall.SIGUSR1)
 	go func() {
 		for sig := range c {
 			// Notify will relay only the signals that we have
 			// registered, so we don't need a "default" in the
 			// switch statement.
 			switch sig {
-			case syscall.SIGKILL, syscall.SIGHUP, syscall.SIGTERM, syscall.SIGINT:
+			case syscall.SIGKILL, syscall.SIGHUP, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT:
 				//2017-12-26
 				s.log.Noticef("get signal:%v, will UnRegister()", sig)
 				s.opts.ConsulUtil.UnRegister()
